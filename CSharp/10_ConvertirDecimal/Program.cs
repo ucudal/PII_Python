3b8347960ff6cb971897fe4d27b6367cbf4dc058
Fernando Machado <fmachadopiriz@outlook.com>
2024-08-20 15:06:34 -0300

    Fixed typo

--- CSharp/10_ConvertirDecimal/Program.cs
+++ CSharp/10_ConvertirDecimal/Program.cs
@@ -1,11 +1,11 @@
-﻿Console.WriteLine(Function.ConvertDecimal("11", 2));
-Console.WriteLine(Function.ConvertDecimal("12", 2));
-Console.WriteLine(Function.ConvertDecimal("101", 2));
-Console.WriteLine(Function.ConvertDecimal("A", 16));
-Console.WriteLine(Function.ConvertDecimal("G", 16));
+﻿Console.WriteLine(Functions.ConvertDecimal("11", 2));
+Console.WriteLine(Functions.ConvertDecimal("12", 2));
+Console.WriteLine(Functions.ConvertDecimal("101", 2));
+Console.WriteLine(Functions.ConvertDecimal("A", 16));
+Console.WriteLine(Functions.ConvertDecimal("G", 16));
 
 // Implementa en esta clase la función que corresponda
-public static class Function
+public static class Functions
 {
     public static int ConvertDecimal(string number, int baseNum)
     {
--- CSharp/1_LeapYear/Program.cs
+++ CSharp/1_LeapYear/Program.cs
@@ -1,8 +1,8 @@
-﻿Console.Write("2000 es bisiesto? :" + Function.IsLeap(2000));
+﻿Console.Write("2000 es bisiesto? :" + Functions.IsLeap(2000));
 
 // Implementa en esta clase la función que corresponda
 
-public static class Function
+public static class Functions
 {
     public static bool IsLeap(int year)
     {
